01c8494c35b4aa8b1d535ca4143c6f49c25e1cdd
“oleharkhipkov” <oleharkhipkov0613@gmail.com>
2020-10-03 17:23:47 -0500

    add dontenv and env vars

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
 
+dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -16,6 +18,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, () => {
-  console.log('server running on port 5000');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
