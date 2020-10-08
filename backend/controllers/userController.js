f37ebcd55fb7e0bcd0ce6557c525b107903629e6
“oleharkhipkov” <oleharkhipkov0613@gmail.com>
2020-10-08 08:53:04 -0500

    add getuser and update user admin routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,25 +105,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Get user profile
-// @route POST /api/users/profile
-// @access Private
-// const getUserProfile = asyncHandler(async (req, res) => {
-//   const user = await User.findById(req.user._id);
-
-//   if (user) {
-//     res.json({
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       isAdmin: user.isAdmin,
-//     });
-//   } else {
-//     res.status(404);
-//     throw new Error('User not found');
-//   }
-// });
-
 // @desc Get all users
 // @route GET /api/users
 // @access Private/Admin
@@ -146,6 +127,46 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Get user by Id
+// @route GET /api/users/:id
+// @access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc Update user
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -153,4 +174,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
