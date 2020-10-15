5083f88c8750782a901c2b78ef8b4c1b2c5d0a74
“oleharkhipkov” <oleharkhipkov0613@gmail.com>
2020-10-15 07:30:42 -0500

    install morgan and create review route and controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -11,6 +12,10 @@ import uploadRoutes from './routes/uploadRoutes.js';
 dotenv.config();
 connectDB();
 const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json());
 
